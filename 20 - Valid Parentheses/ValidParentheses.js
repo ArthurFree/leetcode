e0a16e131ff4947a6ad73aa81e86071e14c8a5e1
ArthurFree <lc940401@gmail.com>
2017-10-24 13:24:52 +0800

    update 20

--- 20 - Valid Parentheses/ValidParentheses.js
+++ 20 - Valid Parentheses/ValidParentheses.js
@@ -3,5 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    var stack = [],
+        map = {
+            ['(']: ')',
+            ['[']: ']',
+            ['{']: '}'
+        };
+    for (var i = 0; i < s.length; i++) {
+        if (!stack.length) {
+            stack.push(s[i]);
+        } else if (s[i] === map[stack[stack.length - 1]]) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
 
+    return !stack.length;
 };
