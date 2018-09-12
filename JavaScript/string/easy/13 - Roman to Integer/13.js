8e1005264b8bc325ca060e1e28260de43458c4ef
ArthurFree <lc940401@gmail.com>
2018-09-12 12:07:04 +0800

    init

---
+++ JavaScript/string/easy/13 - Roman to Integer/13.js
@@ -0,0 +1,21 @@
+const romanToInt = function (r_num) {
+    const RomanNumber = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+    const str = r_num.toString();
+    const result = 0;
+    for (let i = 0, len = str.length; i < len; i++) {
+        const strItem = str.charAt(i);
+        if (i + 1 = str.length)
+        const strNextItem = str.charAt(i+1);
+        if (strItem === 'I') {
+            if ()
+        } else if (strItem === 'X') {} else if (strItem === 'C') {}
+    }
+}
