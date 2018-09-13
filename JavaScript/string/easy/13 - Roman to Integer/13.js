0893325c4747fd44845cad3a543d464dbc8a589d
ArthurFree <lc940401@gmail.com>
2018-09-13 15:34:27 +0800

    add 14 solution

--- JavaScript/string/easy/13 - Roman to Integer/13.js
+++ JavaScript/string/easy/13 - Roman to Integer/13.js
@@ -4,6 +4,7 @@
 
 /**
  * 罗马数字转数字
+ * 执行时间： 236ms
  * @param {*} r_num
  */
 const romanToInt = function (r_num) {
@@ -66,6 +67,68 @@ const romanToInt = function (r_num) {
             }
         } else {
             result += RomanNumber[strItem];
+        }
+    }
+
+    return result;
+}
+
+/**
+ * 罗马数字转数字, 第二种方法
+ * 执行时间：228ms
+ * @param {*} r_num
+ */
+const romanToInt2 = function (r_num) {
+    const RomanNumber = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+    let result = 0;
+
+    for (let i = 0, len = r_num.length; i < len; i++) {
+        const strItem = r_num.charAt(i);
+        const strNextItem = r_num.charAt(i + 1);
+        if (i === len - 1 || RomanNumber[strNextItem] <= RomanNumber[strItem]) {
+            result += RomanNumber[strItem];
+        } else {
+            result -= RomanNumber[strItem];
+        }
+    }
+
+    return result;
+}
+
+/**
+ * 罗马数字转数字, 第三种方法
+ * 执行时间：var - 188ms, const - 252ms
+ * @param {*} r_num
+ */
+var romanToInt3 = function (s_num) {
+    var RomanNumber = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    };
+    var result = 0;
+
+    for (let i = s_num.length - 1; i >= 0; i--) {
+        var strItem = RomanNumber[s_num.charAt(i)];
+        var strPrevItem = RomanNumber[s_num.charAt(i-1)];
+
+        if (i === 0 || strPrevItem >= strItem) {
+            result += strItem;
+        } else {
+            result += strItem - strPrevItem;
+            i--;
         }
     }
 
--- JavaScript/string/easy/14 - Longest Common Prefix/14.js
+++ JavaScript/string/easy/14 - Longest Common Prefix/14.js
@@ -1,3 +1,49 @@
 /**
  * Time: 2018-9-13
  */
+
+/**
+ * 最长公共前缀
+ * 执行时间： 76ms
+ * @param {string[]} arr
+ */
+var longestCommonPrefix = function (arr) {
+    if (arr.length === 0) return '';
+    var firstArrItem = arr.shift();
+    if (arr.length === 0) return firstArrItem;
+    var currLongestPrefix = '';
+    var middle = Math.ceil(firstArrItem.length / 2);
+
+    function getPrefix(str, arr) {
+        if (middle === 0) return;
+        var leftPart = str.slice(0, middle);
+        var hasPrefix = false;
+
+        for (var i = 0, len = arr.length; i < len; i ++) {
+            if (arr[i].indexOf(leftPart) !== 0) {
+                hasPrefix = false;
+                break;
+            } else {
+                hasPrefix = true;
+            }
+        }
+
+        if (!hasPrefix) {
+            if (currLongestPrefix) return;
+            middle--;
+            getPrefix(str, arr);
+        } else {
+            currLongestPrefix = leftPart;
+            middle++;
+            if (middle <= str.length) {
+                // leftPart = str.slice(0, middle + 1);
+                getPrefix(str, arr);
+            }
+        }
+    }
+
+    getPrefix(firstArrItem, arr);
+
+    return currLongestPrefix;
+
+}
