a53db03e2f424c36e4cc37c0e73ae9c54a1feb5e
ArthurFree <lc940401@gmail.com>
2018-09-24 11:22:59 +0800

    add 551 problem

--- JavaScript/string/easy/551 - Student Attendance Record I/551.js
+++ JavaScript/string/easy/551 - Student Attendance Record I/551.js
@@ -4,10 +4,26 @@
 
 /**
  * 反转字符串 II
- * 执行时间： 100ms
+ * 执行时间： 64ms
  * @param {string} s
  * @return {boolean}
  */
 var checkRecord = function(s) {
+    var late = 'LLL';
+    if (s.indexOf(late) > -1) return false;
+    if (s.indexOf('A') !== s.lastIndexOf('A')) return false;
 
+    return true;
+};
+
+/**
+ * 反转字符串 II - 第二种解法
+ * 执行时间： 72ms
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkRecord = function(s) {
+    let reg = /^.*A.*A.*$/;
+    let reg2 = /^.*L{3}.*$/;
+    return !(reg.test(s) || reg2.test(s));
 };
