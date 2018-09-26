a0f6eb20cd728f569323b934f0364a42f61b6aac
ArthurFree <lc940401@gmail.com>
2018-09-26 14:53:09 +0800

    create 686

---
+++ JavaScript/string/easy/686 - Repeated String Match/686.js
@@ -0,0 +1,14 @@
+/**
+ * Time: 2018-9-27
+ */
+
+/**
+ * 重复叠加字符串匹配
+ * 执行时间： ms
+ * @param {string} A
+ * @param {string} B
+ * @return {number}
+ */
+var repeatedStringMatch = function(A, B) {
+
+};
