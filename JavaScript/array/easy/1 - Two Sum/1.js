5f3bd93efb240794cbaa44f50bd7700157b49e4a
ArthurFree <lc940401@gmail.com>
2018-09-29 23:33:04 +0800

    create array folder && create 1

---
+++ JavaScript/array/easy/1 - Two Sum/1.js
@@ -0,0 +1,14 @@
+/**
+ * Time: 2018-9-30
+ */
+
+/**
+ * 两数之和
+ * 执行时间： ms
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+};
