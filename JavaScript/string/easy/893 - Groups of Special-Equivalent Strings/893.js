36435585f04547667bd4ccf2c3f76159ad2c8ee0
ArthurFree <lc940401@gmail.com>
2018-09-29 23:44:03 +0800

    add 893 problem

--- JavaScript/string/easy/893 - Groups of Special-Equivalent Strings/893.js
+++ JavaScript/string/easy/893 - Groups of Special-Equivalent Strings/893.js
@@ -1,5 +1,5 @@
 /**
- * Time: 2018-9-28
+ * Time: 2018-9-29
  */
 
 /**
@@ -9,20 +9,42 @@
  * @return {number}
  */
 var numSpecialEquivGroups = function(A) {
-    var obj = {};
-    for (var i = 0; i < A.length; i++) {
-        var odd = '';
-        var even = '';
-        for (var j = 0; j < A[i].length; j++) {
-            if (i % 2 === 0) {
-                odd += A[i][j];
-            } else {
-                even += A[i][j];
+    return A.reduce((prev, curr) => {
+        let left = '';
+        let right = '';
+        for (let i = 0; i < curr.length; i++) {
+            i % 2 === 0 ? left += curr[i] : right += curr[i];
+        }
+        left = [...left].sort().join('');
+        right = [...right].sort().join('');
+        for (let i = 0; i < prev.length; i++) {
+            if (prev[i][0] === left && prev[i][1] === right) {
+                return prev;
             }
         }
-        odd = odd.split('').sort(function (a, b) { return a.charCodeAt() - b.charCodeAt() }).join('');
-        even = even.split('').sort(function (a, b) { return a.charCodeAt() - b.charCodeAt() }).join('');
-        obj[odd + even] = obj[odd + even] ? obj[odd + even]++ : 1;
-    }
-    return Object.keys(obj).length;
+        prev.push([left, right]);
+        return prev;
+    }, []).length;
 };
+
+/**
+ * 特殊等价字符串组 - 第二种解法
+ * 执行时间：ms
+ * @param {string[]} A
+ * @return {number}
+ */
+const numSpecialEquivGroups = A => {
+    const s = new Set();
+    A.forEach(a => {
+        const odd = [];
+        const even = [];
+        for (let i = 0; i < a.length; i++) {
+            i % 2 ? odd.push(a[i]) : even.push(a[i]);
+        }
+        odd.sort();
+        even.sort();
+        s.add(`${[...odd, ...even]}`);
+    });
+    return s.size;
+}
+// https://leetcode.com/problems/groups-of-special-equivalent-strings/discuss/164241/Javascript-not-the-fastest-but-I-like-it/169674
