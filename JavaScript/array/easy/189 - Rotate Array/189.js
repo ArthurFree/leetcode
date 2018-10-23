83db5d7651f7eee3634e4d08576e098a67785093
ArthurFree <lc940401@gmail.com>
2018-10-23 21:20:56 +0800

    create 189

---
+++ JavaScript/array/easy/189 - Rotate Array/189.js
@@ -0,0 +1,14 @@
+/**
+ * Time: 2018-10-23
+ */
+
+/**
+ * 旋转数组 - 第一种解法
+ * 执行时间：ms
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+
+};
