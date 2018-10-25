47900227e560a53b56f1a7ba83cffd3cceda0204
ArthurFree <lc940401@gmail.com>
2018-10-25 21:37:38 +0800

    create 219

---
+++ JavaScript/array/easy/219 - Contains Duplicate II/219.js
@@ -0,0 +1,14 @@
+/**
+ * Time: 2018-10-25
+ */
+
+/**
+ * 存在重复元素 II - 第一种解法
+ * 执行时间：ms
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+
+};
