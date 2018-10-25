b71ec5c461e0c2f29e6637a5aeaf7564233f62bf
ArthurFree <lc940401@gmail.com>
2018-10-25 21:35:31 +0800

    add 217 problem

--- JavaScript/array/easy/217 - Contains Duplicate/217.js
+++ JavaScript/array/easy/217 - Contains Duplicate/217.js
@@ -1,13 +1,49 @@
 /**
- * Time: 2018-10-24
+ * Time: 2018-10-25
  */
 
 /**
  * 存在重复元素 - 第一种解法
- * 执行时间：ms
+ * 执行时间：3728ms
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+    for (var i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])) {
+            return true;
+        }
+    }
 
+    return false;
+};
+
+/**
+ * 存在重复元素 - 第二种解法
+ * 执行时间：72ms
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    let map = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]] === undefined) {
+            map[nums[i]] = true;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
+ * 存在重复元素 - 第三种解法
+ * 执行时间：84ms
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    var set = new Set(nums);
+    return set.size < nums.length;
 };
