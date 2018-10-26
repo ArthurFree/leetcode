7da90b0c062a48572cec585b3df8e1bbe8bb5119
ArthurFree <lc940401@gmail.com>
2018-10-26 22:30:53 +0800

    add 219 problem

--- JavaScript/array/easy/219 - Contains Duplicate II/219.js
+++ JavaScript/array/easy/219 - Contains Duplicate II/219.js
@@ -1,14 +1,40 @@
 /**
- * Time: 2018-10-25
+ * Time: 2018-10-26
  */
 
 /**
  * 存在重复元素 II - 第一种解法
- * 执行时间：ms
+ * 执行时间：1308ms
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
+    if(nums.length<2){return false;}
+    for(var i=0;i<nums.length;i++){
+       for(var j=i+1;j<=i+k&&j<nums.length;j++){
+           if(nums[i]==nums[j]){return true;}
+       }
+    }
+    return false;
+}
 
+/**
+ * 存在重复元素 II - 第二种解法
+ * 执行时间：64ms
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+let containsNearbyDuplicate = function(nums, k) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; ++i) {
+        let pos = map.get(nums[i]);
+        if (pos !== undefined && i - pos <= k) {
+            return true;
+        }
+        map.set(nums[i], i);
+    }
+
+    return false;
 };
