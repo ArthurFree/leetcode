9634ba54fbe4fcfd608de68486ce4bb9e634b89c
ArthurFree <lc940401@gmail.com>
2018-10-30 17:50:28 +0800

    create 566

---
+++ JavaScript/array/easy/566 - Reshape the Matrix/566.js
@@ -0,0 +1,15 @@
+/**
+ * Time: 2018-10-30
+ */
+
+/**
+ * 重塑矩阵 - 第一种解法
+ * 执行时间：ms
+ * @param {number[][]} nums
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function(nums, r, c) {
+
+};
