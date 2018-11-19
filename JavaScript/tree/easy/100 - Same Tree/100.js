13d6aa3074b6f082f657f992f6b1f3a15ec0ab29
ArthurFree <lc940401@gmail.com>
2018-11-19 17:07:36 +0800

    create tree folder && create 100

---
+++ JavaScript/tree/easy/100 - Same Tree/100.js
@@ -0,0 +1,20 @@
+/**
+ * Time: 2018-11-19
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * 相同的树 - 第一种解法
+ * 执行时间：ms
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+
+};
